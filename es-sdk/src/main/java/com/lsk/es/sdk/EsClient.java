fc9b713c47e9919f9275068d5122c04145d7194f
lvshaokang <lvshaokang@pcitech.com>
2020-08-18 19:58:23 +0800

    commit

---
+++ es-sdk/src/main/java/com/lsk/es/sdk/EsClient.java
@@ -0,0 +1,23 @@
+package com.lsk.es.sdk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.client.RestHighLevelClient;
+
+/**
+ * TODO
+ *
+ * @author Red
+ * @class_name EsClient
+ * @date 2020/08/18
+ */
+@Slf4j
+public class EsClient {
+
+    protected RestHighLevelClient client;
+
+    public static class Builder {
+
+        protected
+    }
+
+}
